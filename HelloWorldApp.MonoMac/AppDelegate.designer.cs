a5eac917164f1eeb869c4818fc47621cbbca311c
moljac <mcvjetko@holisticware.net>
2013-04-06 08:23:33 +0200

    initial comit MT, MonoMac dlls and exes

---
+++ HelloWorldApp.MonoMac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+
+namespace HelloWorldApp.MonoMac
+{
+    // Should subclass MonoMac.AppKit.NSResponder
+    [MonoMac.Foundation.Register("AppDelegate")]
+    public partial class AppDelegate
+    {
+    }
+}
+
