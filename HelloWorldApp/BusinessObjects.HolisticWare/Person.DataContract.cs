62cdf8c09ff2fe033bee11cb44340de89f2535eb
moljac <mcvjetko@holisticware.net>
2013-05-02 10:50:26 +0200

    Refactoring some files to split original and HolisticWare additionals

--- HelloWorldApp/BusinessObjects.HolisticWare/Person.DataContract.cs
+++ HelloWorldApp/BusinessObjects.HolisticWare/Person.DataContract.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 
 
+using System.Runtime.Serialization;
+
+
 namespace HelloWorldApp.BusinessObjects
 {
+	/// <summary>
+	/// System.Runtime.Serialization
+	/// </summary>
 	[DataContract]
 	public partial class Person
 	{
