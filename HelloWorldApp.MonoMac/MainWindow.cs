7d447e6919410a25274e8266ac67937d18066fda
moljac <mcvjetko@holisticware.net>
2013-05-05 12:03:31 +0200

    links

--- HelloWorldApp.MonoMac/MainWindow.cs
+++ HelloWorldApp.MonoMac/MainWindow.cs
@@ -7,29 +7,29 @@ using MonoMac.AppKit;
 
 namespace HelloWorldApp.Mac
 {
-    public partial class MainWindow : MonoMac.AppKit.NSWindow
-    {
+	public partial class MainWindow : MonoMac.AppKit.NSWindow
+	{
 		#region Constructors
 		
-        // Called when created from unmanaged code
-        public MainWindow(IntPtr handle) : base (handle)
-        {
-            Initialize();
-        }
+		// Called when created from unmanaged code
+		public MainWindow(IntPtr handle) : base (handle)
+		{
+			Initialize();
+		}
 		
-        // Called when created directly from a XIB file
-        [Export ("initWithCoder:")]
-        public MainWindow(NSCoder coder) : base (coder)
-        {
-            Initialize();
-        }
+		// Called when created directly from a XIB file
+		[Export ("initWithCoder:")]
+		public MainWindow(NSCoder coder) : base (coder)
+		{
+			Initialize();
+		}
 		
-        // Shared initialization code
-        void Initialize()
-        {
-        }
+		// Shared initialization code
+		void Initialize()
+		{
+		}
 		
 		#endregion
-    }
+	}
 }
 
